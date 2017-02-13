cd688a7d648f0c23315bdc5e92e7125ec3e53c46
ris <ris@riss-MacBook-Pro.local>
2017-02-13 14:19:03 +0800

    add ios-build to build arm version

---
+++ ios-build/ios-build/ios_build.h
@@ -0,0 +1,13 @@
+//
+//  ios_build.h
+//  ios-build
+//
+//  Created by ris on 2/13/17.
+//  Copyright © 2017 ris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ios_build : NSObject
+
+@end
---
+++ ios-build/ios-build/ios_build.m
@@ -0,0 +1,13 @@
+//
+//  ios_build.m
+//  ios-build
+//
+//  Created by ris on 2/13/17.
+//  Copyright © 2017 ris. All rights reserved.
+//
+
+#import "ios_build.h"
+
+@implementation ios_build
+
+@end
